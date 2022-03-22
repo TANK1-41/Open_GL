1e2fe73df30c8ca52174bbc1c46b9ba35fa4a08a
William A <76547127+TANK1-41@users.noreply.github.com>
2022-03-22 18:43:56 -0400

    attempt2

--- src/tests/testCreateSquare.cpp
+++ src/tests/testCreateSquare.cpp
@@ -6,6 +6,8 @@
 #include "VertexBufferLayout.h"
 #include "Renderer.h"
 #include "testCreateSquare.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 test::TestCreateSquare::TestCreateSquare(Renderer* ren)
     :rend(ren),
       va(),
@@ -24,6 +26,9 @@ test::TestCreateSquare::TestCreateSquare(Renderer* ren)
     va.Unbind();
     vb.Unbind();
     ib.Unbind();
+    proj = glm::ortho(0.f, 960.f, 0.f, 540.f, -1.0f, 1.0f);
+    view = glm::translate(glm::mat4(1.0f), glm::vec3(0,0,0));
+
 }
 
 test::TestCreateSquare::~TestCreateSquare()
@@ -32,8 +37,10 @@ test::TestCreateSquare::~TestCreateSquare()
 void test::TestCreateSquare::OnRender()
 {
     shader.Bind();
-    shader.SetUniform4i("u_Texture",0);
     shader.SetUniform4f("u_Color", 0.5f, 0.3f, 0.8f, 1.0f);
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+    glm::mat4 mvp = proj * view * model;
+    shader.SetUniformMat4f("u_MVP", mvp);
     rend->draw(va, ib, shader);
     shader.Unbind();
 }
--- src/tests/testCreateSquare.h
+++ src/tests/testCreateSquare.h
@@ -20,17 +20,23 @@ private:
     Renderer* rend;
     float positions[16]{
             //first triangle
-            -0.5f,-0.5f, 0.0f,0.0f, //0
-            0.5f,-0.5f,1.0f,0.0f,//1
-            0.5f,0.5f, 1.0f,1.0f,//2
+            -50.f, -50.f, 0.0f, 0.0f,//0
+            50.f, -50.f, 1.0f, 0.0f,//1
+            50.f, 50.f, 1.0f, 1.0f,//2
             //second triangle
 
-            -0.5f,0.5f, 0.0f,1.0f//3
+            -50.f, 50.f, 0.0f, 1.0f//3
     };
     unsigned int indices[6]{
             0, 1, 2,//first triangle
             2, 3, 0 //second triangle
     };
+    glm::mat4 proj ;
+    //camera view
+    glm::mat4 view ;
+    //model matrix translation used in game loop
+    glm::vec3 translationA{200,200,0};
+    glm::vec3 translationB{400,200,0};
 };
 
 }
