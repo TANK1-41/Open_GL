36144141e66ee07914d27a385a52ab886787b419
William A <76547127+TANK1-41@users.noreply.github.com>
2022-03-26 15:49:37 -0400

    texture test with files

---
+++ src/tests/testTexture2D.cpp
@@ -0,0 +1,100 @@
+//
+// Created by William Aey on 3/21/2022.
+//
+
+#include "testTexture2D.h"
+#include "Renderer.h"
+#include "imgui/imgui.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+namespace test {
+
+
+    TestTexture2D::TestTexture2D()
+        : m_Proj(glm::ortho(0.f, 960.f, 0.f, 540.f, -1.0f, 1.0f)),m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0,0,0))),
+          m_TranslationA(200,200,0), m_TranslationB(400,200,0)
+    {
+
+        float positions[] = {
+                //first triangle
+                -50.f, -50.f, 0.0f, 0.0f,//0
+                50.f, -50.f, 1.0f, 0.0f,//1
+                50.f, 50.f, 1.0f, 1.0f,//2
+                //second triangle
+
+                -50.f, 50.f, 0.0f, 1.0f//3
+
+        };
+
+        unsigned int indices[]{
+                0, 1, 2,//first triangle
+                2, 3, 0 //second triangle
+        };
+
+        //enables blending for textures
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+        //heaps allocates
+        m_Shader = std::make_unique<Shader>("res/shaders/basic.shader");
+        m_VAO = std::make_unique<VertexArray>();
+        m_IBO = std::make_unique<IndexBuffer>(indices, 6);
+        m_VBO = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
+        m_Texture = std::make_unique<Texture>("res/textures/cherno.png");
+        //vertex buffer layout
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+        m_VAO->AddBuffer(*m_VBO, layout);
+
+
+
+        m_Shader->Bind();
+        m_Texture->Bind();
+        m_Shader->SetUniform4i("u_Texture", 0);
+    }
+
+    TestTexture2D::~TestTexture2D()
+    {
+    }
+
+    void TestTexture2D::OnUpdate(float deltaTime)
+    {
+    }
+
+    void TestTexture2D::OnRender()
+    {
+        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 0.0f));
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+        Renderer renderer;
+        m_Shader->Bind();
+        {
+            //model matrixA
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationA);
+            glm::mat4 mvp = m_Proj * m_View * model;
+            m_Shader->SetUniformMat4f("u_MVP", mvp);
+            renderer.draw(*m_VAO, *m_IBO,*m_Shader);
+        }
+
+        {
+            //model matrixB
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationB);
+            glm::mat4 mvp = m_Proj * m_View * model;
+            m_Shader->SetUniformMat4f("u_MVP", mvp);
+            renderer.draw(*m_VAO, *m_IBO, *m_Shader);
+        }
+
+        m_Shader->SetUniform4i("u_Texture", 0);
+
+    }
+
+    void TestTexture2D::OnImGuiRender()
+    {
+        ImGui::Text("Test Texture 2D");
+        ImGui::SliderFloat3("translationA", &m_TranslationA.x, 0.0f, 960.f);
+        ImGui::SliderFloat3("translationB", &m_TranslationB.x, 0.0f, 960.f);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    }
+}// namespace test
---
+++ src/tests/testTexture2D.h
@@ -0,0 +1,34 @@
+//
+// Created by William Aey on 3/21/2022.
+//
+
+#pragma once
+#include "tests.h"
+#include "texture.h"
+#include "vertexBufferLayout.h"
+#include "vertexBuffer.h"
+#include "memory.h"
+namespace test {
+    class TestTexture2D : public Test {
+    public:
+        TestTexture2D();
+        ~TestTexture2D() override;
+
+        void OnUpdate(float deltaTime) override;
+        void OnRender() override;
+        void OnImGuiRender() override;
+
+    private:
+        std::unique_ptr<VertexArray> m_VAO;
+        std::unique_ptr<VertexBuffer> m_VBO;
+        std::unique_ptr<IndexBuffer> m_IBO;
+        std::unique_ptr<Shader> m_Shader;
+        std::unique_ptr<Texture> m_Texture;
+        //math for aspect ratio m_Proj
+        //camera view matrix
+        glm::mat4 m_Proj,m_View;
+        //model matrix translation used in game loop
+        glm::vec3 m_TranslationA, m_TranslationB;
+
+    };
+}// namespace test
